a08dde9d670c3168f66db525482997594408a6b3
Anand Saini <asaini@fcbg.ch>
2022-04-14 18:34:50 +0200

    added test_network_outputs
    ICLabel network in python and matlab outputs the same values for a common set of features.

--- mne_icalabel/tests/test_iclabel_net.py
+++ mne_icalabel/tests/test_iclabel_net.py
@@ -9,10 +9,12 @@ import numpy as np
 import pytest
 from scipy.io import loadmat
 import torch
+from mne.io import read_raw_fif
 
-
+from mne.preprocessing import ICA
 from mne_icalabel.ica_label import ica_eeg_features
 from mne_icalabel.ica_net import run_iclabel
+from mne_icalabel.ica_net import  ICLabelNet
 
 
 # load in test data for features from original Matlab ICLabel
@@ -20,6 +22,13 @@ ica_file_path = str(files("mne_icalabel.tests").joinpath("data/eeglab_ica.set"))
 ica_raw_file_path = str(files("mne_icalabel.tests").joinpath("data/eeglab_ica_raw.mat"))
 torch_iclabel_path = Path(__file__).parent.parent / 'assets' / 'iclabelNet.pt'
 matconvnet_iclabel_path = Path(__file__).parent / 'data' / 'netICL.mat'
+
+
+sample_input_file = Path(__file__).parent / 'data' / 'ica-test-raw.fif'
+mat_image_features_path = Path(__file__).parent / 'data' / 'matlab_images.mat'
+mat_psds_features_path = Path(__file__).parent / 'data' / 'matlab_psds.mat'
+mat_autocorr_features_path = Path(__file__).parent / 'data' / 'matlab_autocorrs.mat'
+mat_labels_file_path = Path(__file__).parent / 'data' / 'matlab_labels.mat'
 
 
 def test_weights():
@@ -75,7 +84,28 @@ def test_network_outputs():
     Compare that the ICLabel network in python and matlab outputs the same
     values for a common set of features.
     """
-    pass
+    matlab_images = loadmat(mat_image_features_path)['images']
+    matlab_psds = loadmat(mat_psds_features_path)['psds']
+    matlab_autocorrs = loadmat(mat_autocorr_features_path)['autocorrs']
+
+    # Reshaping the matlab extracted features in torch formatting
+    matlab_images = np.transpose(matlab_images, (3, 2, 0, 1))
+    matlab_psds = np.transpose(matlab_psds, (3, 2, 0, 1))
+    matlab_autocorrs = np.transpose(matlab_autocorrs, (3, 2, 0, 1))
+
+    # Converting these in tensors
+    matlab_images = torch.from_numpy(matlab_images).float()
+    matlab_psds = torch.from_numpy(matlab_psds).float()
+    matlab_autocorrs = torch.from_numpy(matlab_autocorrs).float()
+
+    matlab_labels = loadmat(mat_labels_file_path)['labels']
+
+    iclabel_net = ICLabelNet()
+    iclabel_net.load_state_dict(torch.load(torch_iclabel_path))
+    torch_labels = iclabel_net(matlab_images, matlab_psds, matlab_autocorrs)
+    torch_labels = torch_labels.detach().numpy()
+
+    assert np.allclose(matlab_labels, torch_labels, rtol=1e-5, atol=1e-5)
 
 
 def test_labels():

406f06049435c6d5cbf3e40ffdface38317bc40b
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-04-21 17:59:48 +0200

    add compute icaact

--- mne_icalabel/features.py
+++ mne_icalabel/features.py
@@ -1,3 +1,4 @@
+from mne.io import BaseRaw
 from mne.preprocessing import ICA
 import numpy as np
 from numpy.typing import ArrayLike
@@ -17,14 +18,23 @@ def retrieve_eeglab_icawinv(
     Returns
     -------
     icawinv : array
+    weights : array
     """
     n_components = ica.n_components_
     s = np.sqrt(ica.pca_explained_variance_)[:n_components]
     u = ica.unmixing_matrix_ / s
     v = ica.pca_components_[:n_components, :]
-    return np.linalg.pinv((u * s) @ v)
+    weights = (u * s) @ v
+    return np.linalg.pinv(weights), weights
 
 
-def compute_ica_activations():
+def compute_ica_activations(
+        raw: BaseRaw,
+        ica: ICA
+        ) -> ArrayLike:
     """Compute the ICA activations: 'icaact'."""
-    pass
+    icawinv, weights = retrieve_eeglab_icawinv(ica)
+    icasphere = np.eye(icawinv.shape[0])
+    raw_data = raw.get_data(picks=ica.ch_names)
+    icaact = (weights[0:ica.n_components_, :] @ icasphere) @ raw_data
+    return icaact * 1e6
--- mne_icalabel/tests/test_features.py
+++ mne_icalabel/tests/test_features.py
@@ -3,21 +3,34 @@ try:
 except ImportError:
     from importlib_resources import files
 
+from mne.io import read_raw
 from mne.io.eeglab.eeglab import _check_load_mat
 from mne.preprocessing import read_ica_eeglab
 import numpy as np
+from scipy.io import loadmat
 
-from mne_icalabel.features import retrieve_eeglab_icawinv
+from mne_icalabel.features import retrieve_eeglab_icawinv, compute_ica_activations
 
 
 # Raw files with ICA decomposition
 raw_eeglab_path = str(files("mne_icalabel.tests").joinpath("data/sample.set"))
+icaact_eeglab_path = str(files("mne_icalabel.tests").joinpath("data/icaact.mat"))
 
 
 def test_retrieve_eeglab_icawinv():
     """Test that the icawinv is correctly retrieved from an MNE ICA object."""
     ica = read_ica_eeglab(raw_eeglab_path)
-    icawinv = retrieve_eeglab_icawinv(ica)
+    icawinv, _ = retrieve_eeglab_icawinv(ica)
 
     eeg = _check_load_mat(raw_eeglab_path, None)
     assert np.allclose(icawinv, eeg.icawinv)
+
+
+def test_compute_ica_activations():
+    """Test that the icaact is correctly retrieved from an MNE ICA object."""
+    raw = read_raw(raw_eeglab_path)
+    ica = read_ica_eeglab(raw_eeglab_path)
+    icaact = compute_ica_activations(raw, ica)
+
+    icaact_eeglab = loadmat(icaact_eeglab_path)['icaact']
+    assert np.allclose(icaact, icaact_eeglab, rtol=1e-8, atol=1e-4)

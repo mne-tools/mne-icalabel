278734fa1c58fffe252ceef1e1d87b51788f08a8
Adam Li <adam2392@gmail.com>
2022-04-29 19:23:13 -0400

    Add initial commit

--- examples/README.txt
+++
@@ -1,4 +0,0 @@
-Examples
---------
-
-Examples demonstrating ICA labeling.
--- mne_icalabel/iclabel/__init__.py
+++
@@ -1,8 +0,0 @@
-"""ICLabel - An automated electroencephalographic independent component
-classifier, dataset, and website.
-
-This is a python implementation of the EEGLAB plugin 'ICLabel'."""
-
-from .features import get_iclabel_features  # noqa: F401
-from .label_components import label_components  # noqa: F401
-from .network import ICLabelNet, run_iclabel  # noqa: F401

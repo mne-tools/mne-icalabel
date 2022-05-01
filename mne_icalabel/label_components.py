8964a5e50435d22653b426cb45b2bba6a031c7cd
Adam Li <adam2392@gmail.com>
2022-05-01 19:24:22 -0400

    Exp w/ transformer

--- mne_icalabel/label_components.py
+++ mne_icalabel/label_components.py
@@ -1,10 +1,12 @@
 from typing import Union
 
+import numpy as np
 from mne import BaseEpochs
 from mne.io import BaseRaw
 from mne.preprocessing import ICA
 from mne.utils import _validate_type
 from mne.utils.check import _check_option
+from sklearn.base import TransformerMixin, BaseEstimator
 
 from .iclabel import label_components as label_components_iclabel
 from .utils import _validate_inst_and_ica
@@ -12,6 +14,26 @@ from .utils import _validate_inst_and_ica
 methods = {
     "iclabel": label_components_iclabel,
 }
+
+class AutoLabelICA(TransformerMixin):
+    def __init__(self, method:str ='iclabel') -> None:
+        self.method = method
+
+    def fit(self, X, y):
+        pass
+
+    def transform(self, raw, ica):
+        ic_labels = label_components(raw, ica, method=self.method)
+
+        # Afterwards, we can hard threshold the probability values to assign
+        # each component to be kept or not (i.e. it is part of brain signal).
+        # The first component was visually an artifact, which was captured
+        # for certain.
+        not_brain_index = np.argmax(ic_labels, axis=1) != 0
+        exclude_idx = np.argwhere(not_brain_index).squeeze()
+
+        ica.apply(raw, exclude=exclude_idx)
+        return raw
 
 
 def label_components(inst: Union[BaseRaw, BaseEpochs], ica: ICA, method: str):

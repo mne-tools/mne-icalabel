d29da640990f90ea8eab09395c9a8e7baf388103
Adam Li <adam2392@gmail.com>
2022-05-03 15:54:49 -0400

    Bump version to v0.1 for release

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-TAG=$(python -c 'from mne_icalabel.version import VERSION; print("v" + VERSION)')
+TAG=$(python -c 'from mne_icalabel import __version__ as VERSION; print("v" + VERSION)')
 
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
-    python scripts/prepare_changelog.py
+    # python scripts/prepare_changelog.py
     git add -A
     git commit -m "Bump version to $TAG for release" || true && git push
     echo "Creating new git tag $TAG"

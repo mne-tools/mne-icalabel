8892f4089473ab26fcd072a5cb3d063abd82490e
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-05-06 19:42:50 +0200

    Fix typo (#37)
    * fix typo
    
    * better test

--- mne_icalabel/tests/test_label_components.py
+++ mne_icalabel/tests/test_label_components.py
@@ -12,14 +12,14 @@ raw.load_data()
 # preprocess
 raw.filter(l_freq=1.0, h_freq=100.0)
 raw.set_eeg_reference("average")
-# fit ICA
-ica = ICA(n_components=15, method="picard")
-ica.fit(raw)
 
 
 @pytest.mark.filterwarnings("ignore::RuntimeWarning")
-def test_label_components():
+@pytest.mark.parametrize("n_components", (5, 15))
+def test_label_components(n_components):
     """Simple test to check that label_components runs without raising."""
+    ica = ICA(n_components=n_components, method="picard")
+    ica.fit(raw)
     labels = label_components(raw, ica, method="iclabel")
     assert isinstance(labels, dict)
     assert labels["y_pred_proba"].ndim == 1
@@ -28,5 +28,7 @@ def test_label_components():
 
 def test_label_components_with_wrong_arguments():
     """Test that wrong arguments raise."""
+    ica = ICA(n_components=3, method="picard")
+    ica.fit(raw)
     with pytest.raises(ValueError, match="Invalid value for the 'method' parameter"):
         label_components(raw, ica, method="101")

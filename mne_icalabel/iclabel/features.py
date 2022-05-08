186233b6f6108e294816d0f4c27403070958ce5c
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-05-08 21:07:04 +0200

    add test

--- mne_icalabel/iclabel/features.py
+++ mne_icalabel/iclabel/features.py
@@ -63,7 +63,7 @@ def get_iclabel_features(inst: Union[BaseRaw, BaseEpochs], ica: ICA):
     extended_ = 'extended' not in ica.fit_params or ica.fit_params['extended'] is False
     ortho_ = 'ortho' not in ica.fit_params or ica.fit_params['ortho'] is True
     ortho_ = ortho_ if ica.method == 'picard' else False
-    if any(method_, extended_, ortho_):
+    if any((method_, extended_, ortho_)):
         warn(
             f"The provided ICA instance was fitted with a '{ica.method}' algorithm. "
             "ICLabel was designed with extended infomax ICA decompositions. To use the "
--- mne_icalabel/iclabel/tests/test_label_components.py
+++ mne_icalabel/iclabel/tests/test_label_components.py
@@ -1,7 +1,9 @@
 import pytest
+from mne import create_info
 from mne.datasets import sample
-from mne.io import read_raw
+from mne.io import read_raw, RawArray
 from mne.preprocessing import ICA
+import numpy as np
 
 from mne_icalabel.iclabel import iclabel_label_components
 
@@ -22,3 +24,36 @@ def test_label_components():
     """Simple test to check that label_components runs without raising."""
     labels = iclabel_label_components(raw, ica)
     assert labels.shape == (ica.n_components_, 7)
+
+
+def test_warnings():
+    """Test warnings issued when the raw|epochs|ica instance are not using the
+    same algorithm/reference/filters as ICLabel."""
+    data = np.random.randint(low=1, high=10, size=(3, 5000))
+    raw = RawArray(data, create_info(['Fpz', 'CPz', 'Oz'], sfreq=500, ch_types='eeg'))
+    raw.set_montage('standard_1020')
+    raw.filter(1., None)
+
+    # wrong raw, correct ica
+    ica = ICA(n_components=3, method='infomax', fit_params=dict(extended=True))
+    ica.fit(raw)
+    with pytest.warns(RuntimeWarning, match="common average reference"), pytest.warns(RuntimeWarning, match="not filtered between 1 and 100 Hz"):
+        iclabel_label_components(raw, ica)
+    # correct raw, wrong ica
+    raw.filter(1., 100.)
+    raw.set_eeg_reference('average')
+    # infomax
+    ica = ICA(n_components=3, method='infomax', fit_params=dict(extended=False))
+    ica.fit(raw)
+    with pytest.warns(RuntimeWarning, match="designed with extended infomax ICA"):
+        iclabel_label_components(raw, ica)
+    # fastica
+    ica = ICA(n_components=3)
+    ica.fit(raw)
+    with pytest.warns(RuntimeWarning, match="designed with extended infomax ICA"):
+        iclabel_label_components(raw, ica)
+    # picard
+    ica = ICA(n_components=3, method='picard')
+    ica.fit(raw)
+    with pytest.warns(RuntimeWarning, match="designed with extended infomax ICA"):
+        iclabel_label_components(raw, ica)

99cad34e8f02591432eb0bfbbb2b6e427d3d0aa3
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-08-02 22:44:58 +0200

    import features in package __init__

--- mne_icalabel/__init__.py
+++ mne_icalabel/__init__.py
@@ -1,5 +1,6 @@
 """Automatic ICA labeling for MEG, EEG and iEEG data."""
 
 from . import datasets  # noqa: F401
+from . import features  # noqa: F401
 from ._version import __version__  # noqa: F401
 from .label_components import label_components  # noqa: F401

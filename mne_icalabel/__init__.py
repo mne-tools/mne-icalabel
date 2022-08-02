c90036c8fb1b5f2ffa3b9959c07aeade2d59e9fe
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-08-02 22:42:40 +0200

    fix import of dataset

--- mne_icalabel/__init__.py
+++ mne_icalabel/__init__.py
@@ -1,4 +1,5 @@
 """Automatic ICA labeling for MEG, EEG and iEEG data."""
 
+from . import datasets  # noqa: F401
 from ._version import __version__  # noqa: F401
 from .label_components import label_components  # noqa: F401

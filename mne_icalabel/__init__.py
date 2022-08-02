dcd41fa21c71964c12c3682f214505806f5364ee
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-08-02 22:45:46 +0200

    and annotation

--- mne_icalabel/__init__.py
+++ mne_icalabel/__init__.py
@@ -1,5 +1,6 @@
 """Automatic ICA labeling for MEG, EEG and iEEG data."""
 
+from . import annotation  # noqa: F401
 from . import datasets  # noqa: F401
 from . import features  # noqa: F401
 from ._version import __version__  # noqa: F401

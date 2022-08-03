37c9fd0dc1aa173c48dbce09a921d49c6c48807b
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-08-03 14:55:23 +0200

    try with empty pytest_configure

--- mne_icalabel/conftest.py
+++ mne_icalabel/conftest.py
@@ -5,12 +5,4 @@
 
 def pytest_configure(config):
     """Configure pytest options."""
-    warning_lines = r"""
-    error::
-    ignore:.*Setting non-standard config type.*:
-    always::ResourceWarning
-    """  # noqa: E501
-    for warning_line in warning_lines.split("\n"):
-        warning_line = warning_line.strip()
-        if warning_line and not warning_line.startswith("#"):
-            config.addinivalue_line("filterwarnings", warning_line)
+    pass

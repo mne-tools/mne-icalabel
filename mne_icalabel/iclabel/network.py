4b0896291a052925ea2508092a46b4792814400a
Jacob Feitelberg <jfeit@jacobs-mbp.lan>
2023-06-18 22:42:25 -0700

    added onnx code

--- mne_icalabel/iclabel/network.py
+++ mne_icalabel/iclabel/network.py
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from numpy.typing import ArrayLike
+import onnxruntime as ort
 
 
 class _ICLabelNetImg(nn.Module):
@@ -195,7 +196,9 @@ def _format_input(topo: ArrayLike, psd: ArrayLike, autocorr: ArrayLike):
     return formatted_topo, formatted_psd, formatted_autocorr
 
 
-def _format_input_for_torch(topo: ArrayLike, psd: ArrayLike, autocorr: ArrayLike):
+def _format_input_for_torch(
+    topo: ArrayLike, psd: ArrayLike, autocorr: ArrayLike
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """Format the features to the correct shape and type for pytorch."""
     topo = np.transpose(topo, (3, 2, 0, 1))
     psd = np.transpose(psd, (3, 2, 0, 1))
@@ -208,7 +211,18 @@ def _format_input_for_torch(topo: ArrayLike, psd: ArrayLike, autocorr: ArrayLike
     return topo, psd, autocorr
 
 
-def run_iclabel(images: ArrayLike, psds: ArrayLike, autocorr: ArrayLike):
+def _format_input_for_onnx(
+    topo: ArrayLike, psd: ArrayLike, autocorr: ArrayLike
+) -> tuple[ArrayLike, ArrayLike, ArrayLike]:
+    """Format the features to the correct shape and type for ONNX."""
+    topo = np.transpose(topo, (3, 2, 0, 1)).astype(np.float32)
+    psd = np.transpose(psd, (3, 2, 0, 1)).astype(np.float32)
+    autocorr = np.transpose(autocorr, (3, 2, 0, 1)).astype(np.float32)
+
+    return topo, psd, autocorr
+
+
+def run_iclabel(images: ArrayLike, psds: ArrayLike, autocorr: ArrayLike, library: str = "pytorch"):
     """Run the ICLabel network on the provided set of features.
 
     The features are un-formatted and are as-returned by
@@ -230,16 +244,37 @@ def run_iclabel(images: ArrayLike, psds: ArrayLike, autocorr: ArrayLike):
         Columns are ordered with ``'Brain'``, ``'Muscle'``, ``'Eye'``,
         ``'Heart'``, ``'Line Noise'``, ``'Channel Noise'``, and ``'Other'``.
     """
-    ica_network_file = files("mne_icalabel.iclabel").joinpath("assets/iclabelNet.pt")
+    if library == "pytorch":
+        ica_network_file = files("mne_icalabel.iclabel").joinpath("assets/iclabelNet.pt")
 
-    # Get network and load weights
-    iclabel_net = ICLabelNet()
-    iclabel_net.load_state_dict(torch.load(ica_network_file))
+        # Get network and load weights
+        iclabel_net = ICLabelNet()
+        iclabel_net.load_state_dict(torch.load(ica_network_file))
 
-    # Format input and get labels
-    labels = iclabel_net(*_format_input_for_torch(*_format_input(images, psds, autocorr)))
-    labels = labels.detach().numpy()
+        # Format input and get labels
+        labels = iclabel_net(*_format_input_for_torch(*_format_input(images, psds, autocorr)))
+        labels = labels.detach().numpy()
 
-    # outputs are ordered as in
-    # https://github.com/sccn/ICLabel/blob/e8abc99e0c371ff49eff115cf7955fafc7f7969a/iclabel.m#L60-L62
-    return labels
+        # outputs are ordered as in
+        # https://github.com/sccn/ICLabel/blob/e8abc99e0c371ff49eff115cf7955fafc7f7969a/iclabel.m#L60-L62
+        return labels
+    elif library == "onnx":
+        # Get network
+        onnx_network_file = files("mne_icalabel.iclabel").joinpath("assets/ICLabelNet.onnx")
+        ort_session = ort.InferenceSession(onnx_network_file)
+
+        # Format input and get labels
+        topo_npy, psds_npy, autocorr_npy = _format_input_for_onnx(
+            *_format_input(images, psds, autocorr)
+        )
+
+        labels = ort_session.run(
+            None,
+            {"topo": topo_npy, "psds": psds_npy, "autocorr": autocorr_npy},
+        )
+
+        # outputs are ordered as in
+        # https://github.com/sccn/ICLabel/blob/e8abc99e0c371ff49eff115cf7955fafc7f7969a/iclabel.m#L60-L62
+        return labels[0]
+    else:
+        raise ValueError(f"Library {library} is not supported")

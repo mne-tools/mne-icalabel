8efe129332e711b7d4d3da08895b6ed59d64b8b1
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-10-27 13:29:28 +0200

    remove ignore warnings from mne-bids <-> numpy 2.0 compatibility

--- mne_icalabel/conftest.py
+++ mne_icalabel/conftest.py
@@ -8,9 +8,6 @@ def pytest_configure(config):
     # Until MNE 1.6 is the minimum supported version
     # c.f. https://github.com/mne-tools/mne-python/pull/12143
     ignore:Setting non-standard config type.*:RuntimeWarning
-    # mne_bids <-> numpy 2.0
-    # c.f. https://github.com/mne-tools/mne-bids/pull/1184
-    ignore:`in1d` is deprecated. Use `np.isin` instead.:DeprecationWarning
     """
     for warning_line in warnings_lines.split("\n"):
         warning_line = warning_line.strip()

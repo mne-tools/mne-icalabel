49d678fa1fc6ee9bd5daa7d1bab1178bd850b36b
Scott Huberty <seh33@uw.edu>
2025-10-16 16:26:43 -0700

    FIX, DOC: flaky link

--- doc/conf.py
+++ doc/conf.py
@@ -207,7 +207,8 @@ intersphinx_mapping = {
     "pandas": ("https://pandas.pydata.org/pandas-docs/dev", None),
     "pooch": ("https://www.fatiando.org/pooch/latest/", None),
     "python": ("https://docs.python.org/3", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy", None),
+    # Can use stable after https://github.com/scipy/scipy/issues/23757 is addressed
+    "scipy": ("https://scipy.github.io/devdocs/", None),
     "sklearn": ("https://scikit-learn.org/stable", None),
     "torch": ("https://pytorch.org/docs/stable", None),
 }
